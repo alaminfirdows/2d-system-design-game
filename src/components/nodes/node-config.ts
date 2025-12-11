4be1207ff3924ac7c5bd5d46940c2ae5c23c1b34
Al-Amin Firdows <alaminfirdows@gmail.com>
2025-12-11 09:30:28 +0600

    feat: update node configurations to enhance allowed incoming connections

--- src/components/nodes/node-config.ts
+++ src/components/nodes/node-config.ts
@@ -62,40 +62,40 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         type: START,
         label: 'Start',
         shortLabel: 'Start',
-        icon: internetStart, // No icon for start node
+        icon: internetStart,
         iconClassNames: 'bg-white text-background',
         allowedIncoming: [],
-        // allowedOutgoing: [FIREWALL, LOAD_BALANCER, API_GATEWAY, COMPUTE],
-        handles: {
-            out: Position.Bottom,
-        },
+        handles: { out: Position.Bottom },
     },
+
     [LOAD_BALANCER]: {
         type: LOAD_BALANCER,
         label: 'Load Balancer',
         shortLabel: 'LB',
         icon: awsLb,
         iconClassNames: 'bg-purple-900',
-        allowedIncoming: [API_GATEWAY, CDN, FIREWALL],
-        // allowedOutgoing: [COMPUTE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        allowedIncoming: [START, API_GATEWAY, CDN, FIREWALL],
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [COMPUTE]: {
         type: COMPUTE,
         label: 'Compute',
         shortLabel: 'Compute',
         icon: awsEc2,
         iconClassNames: 'bg-orange-700',
-        allowedIncoming: [LOAD_BALANCER, API_GATEWAY, QUEUE, START],
-        // allowedOutgoing: [DATABASE, CACHE, QUEUE, STORAGE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        allowedIncoming: [
+            LOAD_BALANCER,
+            API_GATEWAY,
+            QUEUE,
+            START,
+            DATABASE,
+            CACHE,
+            STORAGE,
+        ],
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [DATABASE]: {
         type: DATABASE,
         label: 'Database',
@@ -103,12 +103,9 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         icon: awsRds,
         iconClassNames: 'bg-blue-700',
         allowedIncoming: [COMPUTE, CACHE],
-        // allowedOutgoing: [COMPUTE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [CACHE]: {
         type: CACHE,
         label: 'Cache',
@@ -116,12 +113,9 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         icon: awsElasticCache,
         iconClassNames: 'bg-blue-700',
         allowedIncoming: [COMPUTE],
-        // allowedOutgoing: [COMPUTE, DATABASE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [QUEUE]: {
         type: QUEUE,
         label: 'Queue',
@@ -129,12 +123,9 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         icon: awsSqs,
         iconClassNames: 'bg-rose-500',
         allowedIncoming: [COMPUTE, API_GATEWAY],
-        // allowedOutgoing: [COMPUTE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [STORAGE]: {
         type: STORAGE,
         label: 'Storage',
@@ -142,12 +133,9 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         icon: awsS3,
         iconClassNames: 'bg-green-700',
         allowedIncoming: [COMPUTE, CDN],
-        // allowedOutgoing: [COMPUTE, CDN],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [FIREWALL]: {
         type: FIREWALL,
         label: 'Firewall',
@@ -155,12 +143,9 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         icon: awsWaf,
         iconClassNames: 'bg-red-500',
         allowedIncoming: [START],
-        // allowedOutgoing: [LOAD_BALANCER, API_GATEWAY, CDN],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [CDN]: {
         type: CDN,
         label: 'CDN',
@@ -168,24 +153,17 @@ export const nodeConfigs: Record<NodeType, NodeConfig> = {
         icon: awsCloudfront,
         iconClassNames: 'bg-purple-700',
         allowedIncoming: [FIREWALL, STORAGE],
-        // allowedOutgoing: [LOAD_BALANCER, STORAGE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        handles: { in: Position.Top, out: Position.Bottom },
     },
+
     [API_GATEWAY]: {
         type: API_GATEWAY,
         label: 'API Gateway',
         shortLabel: 'Gateway',
         icon: awsApiGateway,
         iconClassNames: 'bg-rose-700',
-        allowedIncoming: [FIREWALL],
-        // allowedOutgoing: [LOAD_BALANCER, COMPUTE, QUEUE],
-        handles: {
-            in: Position.Top,
-            out: Position.Bottom,
-        },
+        allowedIncoming: [FIREWALL, START],
+        handles: { in: Position.Top, out: Position.Bottom },
     },
 };
 
